ee3e84fbe2caa9bdd5abd7a9f7dab64dd4986def
zhouzach <769087026@qq.com>
2018-05-31 14:17:28 +0800

    add pom.xml

---
+++ src/main/java/org/zach/fundamentals/TestConstructor.java
@@ -0,0 +1,12 @@
+package org.zach.fundamentals;
+
+class TestConstructor {
+    int x;
+}
+
+class Demo {
+    public static void main(String[] args) {
+        TestConstructor constructor = new TestConstructor();
+        System.out.print(constructor.x);
+    }
+}
