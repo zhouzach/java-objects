4ad930cf2d8585a17f471907489421e0aba4f553
zhouzach <769087026@qq.com>
2018-07-04 16:12:25 +0800

    refactor io api

--- src/main/java/org/zach/io/CopyFile.java
+++ src/main/java/org/zach/io/CopyFile.java
@@ -3,9 +3,6 @@ package org.zach.io;
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Paths;
 
 public class CopyFile {
     public static void main(String[] args) {
@@ -19,7 +16,7 @@ public class CopyFile {
                 if (i != -1) fout.write(i);
             } while (i != -1);
         } catch (IOException exp) {
-            System.out.println(exp);
+            System.out.println(exp.getLocalizedMessage());
         }
     }
 }
@@ -51,51 +48,11 @@ class CopyBinaryFile {
 
 class FileUtil {
     public static void main(String[] agrs) {
-        String s = "http://192.168.1.17:8008/upload/helle.txt";
 
-//        try(FileInputStream fin = new FileInputStream("source.txt")) {
-//            FileUtils.copyInputStreamToFile(fin, new File( "hello.txt"));
-//        } catch(IOException exp) {
-//
-//        }
-
-
-
-        try {
-
-            URL url = new URL(s);
-            URLConnection connection = url.openConnection();
-            connection.setDoOutput(true);
-
-            OutputStreamWriter out = new OutputStreamWriter(
-                    connection.getOutputStream());
-            out.write("string=");
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        } finally {
-
+        try (FileInputStream fin = new FileInputStream("source.txt")) {
+            FileUtils.copyInputStreamToFile(fin, new File("hello.txt"));
+        } catch (IOException exp) {
+            System.out.println(exp.getLocalizedMessage());
         }
-    }
-}
-
-class Loop {
-    public static void main(String[] args) {
-
-        String s1 = Paths.get("/Users").normalize().toString();
-        String s2 = Paths.get("/Users/Zach").toString();
-        System.out.println(s1);
-        System.out.println("*************");
-        System.out.println(s2);
-    }
-
-    static boolean test() {
-        do {
-            do {
-
-                return true;
-            } while (true);
-        } while (true);
     }
 }
