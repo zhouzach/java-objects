ce629a208435762f6ba16437fdf118305618a020
Zhou Zach <wander669@gmail.com>
2019-02-25 09:40:04 +0800

    lambad style

--- src/main/java/org/zach/async/Java8Future.java
+++ src/main/java/org/zach/async/Java8Future.java
@@ -10,18 +10,15 @@ public class Java8Future {
     public static void main(String[] args) throws Throwable, ExecutionException {
         // 两个线程的线程池
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(new Supplier<String>() {
-            @Override
-            public String get() {
-                System.out.println("task started!");
-                try {
-                    //模拟耗时操作
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                return "task finished!";
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("task started!");
+            try {
+                //模拟耗时操作
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            return "task finished!";
         }, executor);
 
         //采用lambada的实现方式
