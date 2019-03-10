aa4243df174d85312ab045bd83240e5269136c26
Zhou Zach <wander669@gmail.com>
2019-03-10 14:27:51 +0800

    hashmap iterate first and forEachRemaining D

--- src/main/java/org/zach/collections/HashMapObj.java
+++ src/main/java/org/zach/collections/HashMapObj.java
@@ -1,17 +1,41 @@
 package org.zach.collections;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class HashMapObj {
     public static void main(String[] args) {
-        HashMap<String, Long> discountCountMap = new HashMap<>();
-        discountCountMap.put("d",3L);
-        discountCountMap.put("d",6L);
+        HashMap<String, Long> map = new HashMap<>();
+        map.put("d ",3L);
 
-        Object[] keys = discountCountMap.keySet().toArray();
+        map.put("b ",6L);
+        map.put("h ",6L);
+        map.put("j ",5L);
+        map.put("z ",5L);
+
+        Object[] keys = map.keySet().toArray();
         for (int i = 0; i < keys.length; i++) {
             System.out.print(keys[i]);
-            System.out.println(discountCountMap.get(keys[i]));
+            System.out.println(map.get(keys[i]));
         }
+
+
+        System.out.println("-------------");
+        Iterator<Map.Entry<String, Long>> iterator = map.entrySet().iterator();
+        Map.Entry<String, Long> first = iterator.next();
+        iterator.forEachRemaining(stringLongEntry -> {
+            System.out.print(stringLongEntry.getKey());
+            System.out.println(stringLongEntry.getValue());
+
+
+        });
+        System.out.println("-------------");
+
+        while (iterator.hasNext()) {
+            Map.Entry<String, Long> entry = iterator.next();
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
     }
 }
---
+++ src/main/java/org/zach/number/IntergerObj.java
@@ -0,0 +1,12 @@
+package org.zach.number;
+
+public class IntergerObj {
+
+    public static void main(String[] args) {
+        Integer i = Integer.parseInt("20180201");
+        String year = "20180201".substring(4, 6);
+        System.out.println(i);
+        System.out.println(year);
+
+    }
+}
