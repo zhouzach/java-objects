a0a55ecf7f0d60420b9fae31bfa647345070e04b
Zhou Zach <wander669@gmail.com>
2019-03-13 14:15:56 +0800

    iterate hashmap

--- src/main/java/org/zach/collections/HashMapObj.java
+++ src/main/java/org/zach/collections/HashMapObj.java
@@ -8,12 +8,12 @@ import java.util.Map;
 public class HashMapObj {
     public static void main(String[] args) {
         HashMap<String, Long> map = new HashMap<>();
-        map.put("d ",3L);
+        map.put("d ", 3L);
 
-        map.put("b ",6L);
-        map.put("h ",6L);
-        map.put("j ",5L);
-        map.put("z ",5L);
+        map.put("b ", 6L);
+        map.put("h ", 6L);
+        map.put("j ", 5L);
+        map.put("z ", 5L);
 
         Object[] keys = map.keySet().toArray();
         for (int i = 0; i < keys.length; i++) {
@@ -40,14 +40,44 @@ public class HashMapObj {
 
     }
 
-    public static void iterateByForEach(Map<String, String> map){
+    /**
+     * efficiently iterate over each entry in a Java Map
+     *
+     * @param map
+     */
+    public static void iterateByForEntry(Map<String, String> map) {
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
+
+
+        for (String key : map.keySet()) {
+            System.out.println("Key = " + key);
+        }
+    }
+
+
+    public static void iterateByForEach(Map<String, String> map) {
         map.forEach((k, v) -> {
             System.out.println(k + ":" + v);
 
         });
     }
 
-    public static String mkStringByIterator(Map<String, String> map){
+    public static void iterateByIterator(Map<String, String> map) {
+
+        Iterator<Map.Entry<String, String>> entries = map.entrySet().iterator();
+
+        while (entries.hasNext()) {
+
+            Map.Entry<String, String> entry = entries.next();
+
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+
+        }
+    }
+
+    public static String mkStringByIterator(Map<String, String> map) {
         StringBuilder builder = new StringBuilder();
 
         Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
@@ -66,7 +96,12 @@ public class HashMapObj {
     }
 
 
-    public static String mkStringByForLoop(Map<String, String> map){
+    /**
+     * 通过键找值，效率低
+     * @param map
+     * @return
+     */
+    public static String mkStringByForLoop(Map<String, String> map) {
         StringBuilder builder = new StringBuilder();
 
         Object[] keys = map.keySet().toArray();
