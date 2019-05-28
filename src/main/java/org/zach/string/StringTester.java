4ac612f3485de215db57f1ef17e8038a7ae62efa
Zhou Zach <wander669@gmail.com>
2019-05-28 18:24:46 +0800

    StringBuilder clear data

--- src/main/java/org/zach/string/StringTester.java
+++ src/main/java/org/zach/string/StringTester.java
@@ -6,9 +6,17 @@ public class StringTester {
     public static void main(String[] args) {
         String p ="";
         String[] shell = new String[]{"hello", p};
-        System.out.println(p);
-        System.out.println(shell.length);
-        System.out.println(Arrays.toString(shell));
+//        System.out.println(p);
+//        System.out.println(shell.length);
+//        System.out.println(Arrays.toString(shell));
+//
+        StringBuilder builder = new StringBuilder();
+        builder.append("a").append("b");
+        System.out.println(builder.toString());
+        System.out.println("-------");
+        builder.setLength(0);
+        System.out.println(builder.toString());
+
 
     }
 }
