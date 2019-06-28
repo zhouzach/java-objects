f9ff17567ca655d557fe308f025e3c6fd868e104
Zhou Zach <wander669@gmail.com>
2019-06-28 17:44:39 +0800

    final variable

---
+++ src/main/java/org/zach/fundamentals/FinalObj.java
@@ -0,0 +1,18 @@
+package org.zach.fundamentals;
+
+public class FinalObj {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void init() {
+        final String s;
+        s = "final";
+
+        // final variable can be assigned only one
+//        s="modify";
+
+
+    }
+}
